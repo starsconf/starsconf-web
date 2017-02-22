07bd292db24422dffe5e1281316f811fba240417
Ricardo Alfaro <ricardoalfarog@gmail.com>
2017-02-22 17:10:34 -0300

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,12 @@ var images = Array(
     "assets/img/wallpaper-site_9.jpg",
     "assets/img/wallpaper-site_6.jpg",
     "assets/img/wallpaper-site_5.jpg",
-    "assets/img/wallpaper-site_8.jpg",
+    "assets/img/wallpaper-site_3.jpg",
     "assets/img/wallpaper-site_7.jpg",
     "assets/img/wallpaper-site_10.jpg",
     "assets/img/wallpaper-site_11.jpg",
     "assets/img/wallpaper-site_8.jpg",
-    "assets/img/wallpaper-site_9.jpg",
+    "assets/img/wallpaper-site_2.jpg",
     "assets/img/wallpaper-site_14.jpg",
     "assets/img/wallpaper-site_15.jpg",
     "assets/img/wallpaper-site_16.jpg",
@@ -34,33 +34,40 @@ var currimg = 0;
 
 
 $(document).ready(function(){
-   //CHANGE BACKGROUND IMAGES
-   
-    function loadimg(){   
-       $('#background').animate({ opacity: 1 }, 800,function(){
-            //finished animating, minifade out and fade new back in           
-            $('#background').animate({ opacity: 0.7 }, 400,function(){ 
-                currimg++;
-                if(currimg > images.length-1){
-                    currimg=0;
-                }
-                var newimage = images[currimg];
-            
-                //swap out bg src                
-                $('#background').css("background-image", "url("+newimage+")"); 
-                //animate fully back in
-                $('#background').animate({ opacity: 1 }, 800,function(){
-                    //set timer for next
-                    setTimeout(loadimg,4000);
+    function isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+
+    if (!isMobile()) {
+        //CHANGE BACKGROUND IMAGES
+       
+        function loadimg(){   
+           $('#background').animate({ opacity: 1 }, 800,function(){
+                //finished animating, minifade out and fade new back in           
+                $('#background').animate({ opacity: 0.7 }, 400,function(){ 
+                    currimg++;
+                    if(currimg > images.length-1){
+                        currimg=0;
+                    }
+                    var newimage = images[currimg];
+                
+                    //swap out bg src                
+                    $('#background').css("background-image", "url("+newimage+")"); 
+                    //animate fully back in
+                    $('#background').animate({ opacity: 1 }, 800,function(){
+                        //set timer for next
+                        setTimeout(loadimg,4000);
+                    });
+
                 });
-
+            
             });
-        
-        });
-     }
-     setTimeout(loadimg,4000);
-     
+         }
+         setTimeout(loadimg,4000);
+         
+    
 
+    }
      //Fix header
     $(window).scroll(function() {
         if ($(this).scrollTop() > 80) {
