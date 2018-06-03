3f6520daca142de63bbb948d1637df0c3186fa6b
Ricardo Alfaro <ricardoalfarog@gmail.com>
2018-06-03 17:55:03 -0400

    add discipline code page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -18,33 +18,33 @@ window.onclick = function(event) {
 
 // Utilities
 function checkArray(arr) {
-    if (!Array.isArray(arr) || !arr.length) {
-        return false;
-    }
+  if (!Array.isArray(arr) || !arr.length) {
+    return false;
+  }
 }
 
 // Slideshow images to preload
 var imagesPath = [
-    'images/wallpaper-site_1.jpg',
-    'images/wallpaper-site_2.jpg',
-    'images/wallpaper-site_3.jpg',
-    'images/wallpaper-site_4.jpg',
-    'images/wallpaper-site_5.jpg',
-    'images/wallpaper-site_6.jpg',
-    'images/wallpaper-site_7.jpg',
-    'images/wallpaper-site_8.jpg',
-    'images/wallpaper-site_9.jpg',
-    'images/wallpaper-site_10.jpg',
-    'images/wallpaper-site_11.jpg',
-    'images/wallpaper-site_12.jpg',
-    'images/wallpaper-site_13.jpg',
-    'images/wallpaper-site_14.jpg',
-    'images/wallpaper-site_15.jpg',
-    'images/wallpaper-site_16.jpg',
-    'images/wallpaper-site_17.jpg',
-    'images/wallpaper-site_18.jpg',
-    'images/wallpaper-site_19.jpg',
-    'images/wallpaper-site_20.jpg'
+'images/wallpaper-site_1.jpg',
+'images/wallpaper-site_2.jpg',
+'images/wallpaper-site_3.jpg',
+'images/wallpaper-site_4.jpg',
+'images/wallpaper-site_5.jpg',
+'images/wallpaper-site_6.jpg',
+'images/wallpaper-site_7.jpg',
+'images/wallpaper-site_8.jpg',
+'images/wallpaper-site_9.jpg',
+'images/wallpaper-site_10.jpg',
+'images/wallpaper-site_11.jpg',
+'images/wallpaper-site_12.jpg',
+'images/wallpaper-site_13.jpg',
+'images/wallpaper-site_14.jpg',
+'images/wallpaper-site_15.jpg',
+'images/wallpaper-site_16.jpg',
+'images/wallpaper-site_17.jpg',
+'images/wallpaper-site_18.jpg',
+'images/wallpaper-site_19.jpg',
+'images/wallpaper-site_20.jpg'
 ];
 
 // Store newly created images
@@ -52,12 +52,12 @@ var images = [];
 
 // Image preloader
 function preloader(arr) {
-    checkArray(arr);
-    for (var i = 0; i < arr.length; i++) {
-        var img = new Image();
-        img.src = arr[i];
-        images[i] = img;
-    }
+  checkArray(arr);
+  for (var i = 0; i < arr.length; i++) {
+    var img = new Image();
+    img.src = arr[i];
+    images[i] = img;
+  }
 }
 
 // Preload images
@@ -69,20 +69,20 @@ preloader(imagesPath);
 
     // Check if the user agent is mobile
     function isMobile() {
-        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     }
 
     // Check if the element is visible in the viewport
     function isInViewport(element) {
-        if (!element || element.nodeType != 1) {
-            return false;
-        }
+      if (!element || element.nodeType != 1) {
+        return false;
+      }
 
-        var bounds = element.getBoundingClientRect();
+      var bounds = element.getBoundingClientRect();
 
-        return (
-            bounds.top < window.innerHeight && 
-            bounds.bottom > 0
+      return (
+        bounds.top < window.innerHeight && 
+        bounds.bottom > 0
         );
     }
 
@@ -97,78 +97,88 @@ preloader(imagesPath);
 
     // Play slideshow
     function playSlideshow(el, images) {
-        checkArray(images);
-        if (!el || el.nodeType != 1) {
-            return false;
-        }
+      checkArray(images);
+      if (!el || el.nodeType != 1) {
+        return false;
+      }
         // Add transition to the block
         el.style.transition = 'background-color 1s ease-out';
         // Use imagesLoaded to check if all images loaded
         var imgLoad = imagesLoaded(images);
         // Don't start until all images are ready
         imgLoad.on('done', function() {
-            slideAnimate = setInterval(function() {
-                current = (current != images.length - 1) ? current + 1 : 0;
-                for (var i = 0; i < images.length; i++) {
-                    el.style.backgroundColor="black";
-                    setTimeout(function() {
-                        el.style.backgroundColor="rgba(0,0,0,0.2)";
-                        el.style.backgroundImage="url('" + images[current].src + "')";
-                    }, 700)
-                }
-            }, 6000);
+          slideAnimate = setInterval(function() {
+            current = (current != images.length - 1) ? current + 1 : 0;
+            for (var i = 0; i < images.length; i++) {
+              el.style.backgroundColor="black";
+              setTimeout(function() {
+                el.style.backgroundColor="rgba(0,0,0,0.2)";
+                el.style.backgroundImage="url('" + images[current].src + "')";
+              }, 700)
+            }
+          }, 6000);
         })
-    }
+      }
 
     // Stop slideshow
     function stopSlideshow() {
-        clearInterval(slideAnimate);
+      clearInterval(slideAnimate);
     }
 
     // Initialize slideshow only if isn't mobile
     if (!isMobile()) {
-       playSlideshow(slideshow, images); 
+     playSlideshow(slideshow, images); 
+   }
+
+    //Fix Elements
+    function stickyElements() {
+      let scroll = window.scrollY;
+      let header = document.querySelector('header');
+      let footer = document.querySelector('footer');
+      let cta = document.querySelector('.cta');
+
+      let headerHeight = header.offsetHeight;
+      let sizeFooterHeight = footer.offsetHeight;
+
+      if (scroll > (headerHeight / 2)) {
+        header.classList.add('header-fixed');
+      } else {
+        header.classList.remove('header-fixed');
+      }
+
+      if (scroll > sizeFooterHeight) {
+        footer.classList.add('scrolling');
+      } else {
+        footer.classList.remove('scrolling');
+      }
+
+      if (scroll > 268) {
+        cta.classList.add('button-fixed');
+      } else {
+        cta.classList.remove('button-fixed');
+      }
+
     }
 
-    // Fix navbar
-    function stickyNavbar() {
-        var scroll = window.scrollY;
-        var navbar = document.querySelector('.top-header');
-        var header = document.querySelector('.home-header');
-
-        if (!navbar || !header) {
-            return false;
-        }
-
-        var navbarHeight = navbar.offsetHeight;
-        var headerHeight = header.offsetHeight - navbarHeight;
-
-        if (scroll > (navbarHeight / 2) && scroll < headerHeight) {
-            navbar.classList.remove('full-bg');
-            navbar.classList.add('not-bg');
-        } else if (scroll > headerHeight) {
-            navbar.classList.add('full-bg');
-        } else {
-            navbar.classList.remove('full-bg', 'not-bg');
-        }
-    }
 
     // Listen to scroll event with throttle
     window.addEventListener('scroll', _.throttle(function() {
+      // Sticky navbar on scroll
+      //stickyNavbar();
 
-        // Sticky navbar on scroll
-        stickyNavbar();
+      // Elements fixed on scroll
+      stickyElements();
 
-        // Disable slideshow when out of the viewport
-        if (isInViewport(slideshow)) {
-            if (!slideState && !isMobile()) {
-                playSlideshow(slideshow, images);
-                slideState = true;
-            }
-        } else {
-            stopSlideshow();
-            slideState = false;
+      // Disable slideshow when out of the viewport
+      if (isInViewport(slideshow)) {
+        if (!slideState && !isMobile()) {
+          playSlideshow(slideshow, images);
+          slideState = true;
         }
+      } else {
+        stopSlideshow();
+        slideState = false;
+      }
     }, 100), false);
 
     /*
@@ -179,10 +189,10 @@ preloader(imagesPath);
 
     //Responsive Nav Menu
     $('.toggle-nav-mobile').click(function(){
-        $('.header-nav').addClass('display');
+      $('.header-nav').addClass('display');
     })
     $('.bg').click(function(){
-        $('.header-nav').removeClass('display');
+      $('.header-nav').removeClass('display');
     })
 
     // Scrollspy
@@ -194,4 +204,4 @@ preloader(imagesPath);
     })
 
 
-})(jQuery);
+  })(jQuery);
